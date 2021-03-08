2bde20d20deed17f9695da9ed4afd23b2b531407
ak-matsu <sandbox.matsu@gmail.com>
2021-03-08 22:42:38 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,15 @@ class PostsController < ApplicationController
   # 既読の操作を行ったときに実行されるアクション
   def checked
     post = Post.find(params[:id])
+    # 既読であるか否かを判別
     if post.checked
       post.update(checked: false)
     else
       post.update(checked: true)
     end
 
+    # 更新したレコードをitem = Post.find(params[:id])で取得し直し、
+    # render json:{ post: item }でJSON形式（データ）としてchecked.jsに返却
     item = Post.find(params[:id])
     render json: {  post: item}
   end
