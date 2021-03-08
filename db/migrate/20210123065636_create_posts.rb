25ae2759f125be408f89e4f440fb1e848b198b5c
ak-matsu <sandbox.matsu@gmail.com>
2021-03-08 21:27:52 +0900

    migrate にてカラムを追加

--- db/migrate/20210123065636_create_posts.rb
+++ db/migrate/20210123065636_create_posts.rb
@@ -2,6 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
+      # 既読機能実装時に既読、未読の判断
+      t.boolean :checked
       t.timestamps
     end
   end
