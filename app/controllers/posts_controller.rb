48e16ae5ffc01b94858fd61e41f7ff491e9043b7
ak-matsu <sandbox.matsu@gmail.com>
2021-03-08 22:36:07 +0900

    checkedアクションを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,22 @@ class PostsController < ApplicationController
     #.orderメソッド(id:"DESC）でメモの順番を降順へ表示
     @posts = Post.all.order(id:"DESC")
   end
-  
+
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-end
 
-# コントローラー
+  # 既読の操作を行ったときに実行されるアクション
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: {  post: item}
+  end
+end
