e0ffae953dcdca74515336f737bad005ecfc069e
ak-matsu <sandbox.matsu@gmail.com>
2021-03-09 22:02:03 +0900

    リクエストの送信準備完了

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -4,7 +4,21 @@ function check(){
   // 要素1つずつに対して「クリック」した際、動作する処理を記述
   posts.forEach(function(post){
     // 処理としてaddEventListenerメソッドを使用、引数にclickの指定をする
-    post.addEventListener("click",() => {});
+    post.addEventListener("click",() => {
+      // ここにクリックした時に行う「何らかの処理」を記述していく
+      // getAttributeで属性値を取得、メモのidを取得
+      const postID = post.getAttribute("data-id");
+      // エンドポイントを呼び出すためXMLHttpRequestをしゆしてHTTPリクエストを送る。
+      // オブジェクトを生成
+      const XHR =  new XMLHttpRequest();
+      // httpメソッド:GET,path指定:/posts/${postId},非同期通信のON/OFF:true
+      XHR.open("GET", `/posts/${postId}`,true);
+      // リクエストを送る際に予め、レスポンスしてほしい情報の形式を指定する。
+      // レスポンスの形式はjsonとする。
+      XHR.responseType = "json";
+      // .sendメソッドを記述することで初めてリクエストが行える。
+      XHR.send();
+    });
   });
 }
 window.addEventListener("load",check);
