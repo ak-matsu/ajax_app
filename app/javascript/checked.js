39cfd1fcc5a90aa8f052285a3ea88eacd3df15f4
ak-matsu <sandbox.matsu@gmail.com>
2021-03-09 21:23:38 +0900

    要素1つずつに対して、『クリック』した際に動作するイベント駆動」を設定

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,5 +1,10 @@
 function check(){
   // querySelectorAllメソッドで、postをクラス名にもつ要素を取得できます。postというクラス名を持つ要素はメモの数だけ存在
   const posts = document.querySelectorAll(".post");
+  // 要素1つずつに対して「クリック」した際、動作する処理を記述
+  posts.forEach(function(post){
+    // 処理としてaddEventListenerメソッドを使用、引数にclickの指定をする
+    post.addEventListener("click",() => {});
+  });
 }
 window.addEventListener("load",check);
