98526d38954b39064a203d7342154023389b1d50
ak-matsu <sandbox.matsu@gmail.com>
2021-03-09 22:33:33 +0900

    binding.pryがうまく止まらない

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,8 @@ class PostsController < ApplicationController
   def index
     #すべてのレコードを@postsに代入
     #.orderメソッド(id:"DESC）でメモの順番を降順へ表示
-    @posts = Post.all.order(id:"DESC")
+    @posts = Post.all.order(id: "DESC")
   end
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
@@ -12,6 +11,7 @@ class PostsController < ApplicationController
 
   # 既読の操作を行ったときに実行されるアクション
   def checked
+    binding.pry
     post = Post.find(params[:id])
     # 既読であるか否かを判別
     if post.checked
@@ -19,10 +19,10 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
-
     # 更新したレコードをitem = Post.find(params[:id])で取得し直し、
     # render json:{ post: item }でJSON形式（データ）としてchecked.jsに返却
     item = Post.find(params[:id])
-    render json: {  post: item}
+    render json: { post: item }
   end
+
 end
--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,18 +1,18 @@
-function check(){
+function check() {
   // querySelectorAllメソッドで、postをクラス名にもつ要素を取得できます。postというクラス名を持つ要素はメモの数だけ存在
   const posts = document.querySelectorAll(".post");
   // 要素1つずつに対して「クリック」した際、動作する処理を記述
-  posts.forEach(function(post){
+  posts.forEach(function (post) {
     // 処理としてaddEventListenerメソッドを使用、引数にclickの指定をする
-    post.addEventListener("click",() => {
+    post.addEventListener("click", () => {
       // ここにクリックした時に行う「何らかの処理」を記述していく
       // getAttributeで属性値を取得、メモのidを取得
-      const postID = post.getAttribute("data-id");
+      const postId = post.getAttribute("data-id");
       // エンドポイントを呼び出すためXMLHttpRequestをしゆしてHTTPリクエストを送る。
       // オブジェクトを生成
-      const XHR =  new XMLHttpRequest();
+      const XHR = new XMLHttpRequest();
       // httpメソッド:GET,path指定:/posts/${postId},非同期通信のON/OFF:true
-      XHR.open("GET", `/posts/${postId}`,true);
+      XHR.open("GET", `/posts/${postId}`, true);
       // リクエストを送る際に予め、レスポンスしてほしい情報の形式を指定する。
       // レスポンスの形式はjsonとする。
       XHR.responseType = "json";
@@ -21,4 +21,4 @@ function check(){
     });
   });
 }
-window.addEventListener("load",check);
+window.addEventListener("load", check);
