b585f691d5b16065a07716a33ae5cb04f5729079
ak-matsu <sandbox.matsu@gmail.com>
2021-03-09 21:17:56 +0900

    クリック時のイベントを定義

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,2 +1,5 @@
-function check(){}
+function check(){
+  // querySelectorAllメソッドで、postをクラス名にもつ要素を取得できます。postというクラス名を持つ要素はメモの数だけ存在
+  const posts = document.querySelectorAll(".post");
+}
 window.addEventListener("load",check);
