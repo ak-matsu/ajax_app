97951f5279aef5432dae014392853d3911fbcbad
ak-matsu <sandbox.matsu@gmail.com>
2021-03-10 21:05:35 +0900

    リクエストを送信

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,8 @@
-function memo(){}
+function memo(){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click",(e) => {
+    const XHR = new XMLHttpRequest();
+  });
+}
 // Windowを読み込んだ時に実行
 window.addEventListener("load",memo);
