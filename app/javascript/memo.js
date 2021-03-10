d3d5dc3ecca282a801596bd040433f10d4f30827
ak-matsu <sandbox.matsu@gmail.com>
2021-03-10 21:08:11 +0900

    memo.jsにXMLHttpRequestを初期化する記述

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -2,6 +2,7 @@ function memo(){
   const submit = document.getElementById("submit");
   submit.addEventListener("click",(e) => {
     const XHR = new XMLHttpRequest();
+    XHR.open("POST","/posts",true);
   });
 }
 // Windowを読み込んだ時に実行
