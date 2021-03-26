dd81c810aaa33fdba98cd0fe14535ef1cb0b828b
ak-matsu <sandbox.matsu@gmail.com>
2021-03-26 21:28:27 +0900

    更新

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,65 +1,42 @@
 function check() {
-  // querySelectorAllメソッドで、postをクラス名にもつ要素を取得できます。
-  // postというクラス名を持つ要素はメモの数だけ存在
+  // 表示されているすべてのメモを取得している
   const posts = document.querySelectorAll(".post");
   posts.forEach(function (post) {
-    if (post.getAttribute("data-load") != null) {
+     if (post.getAttribute("data-load") != null) {
       return null;
     }
     post.setAttribute("data-load", "true");
-
-    // 要素1つずつに対して「クリック」した際、動作する処理を記述
-    // 処理としてaddEventListenerメソッドを使用、引数にclickの指定をする
+    // メモをクリックした場合に実行する処理を定義している
     post.addEventListener("click", () => {
-
-      // ここにクリックした時に行う「何らかの処理」を記述していく
-      // getAttributeで属性値を取得、メモのidを取得
+      // どのメモをクリックしたのか、カスタムデータを利用して取得している
       const postId = post.getAttribute("data-id");
-
-      // エンドポイントを呼び出すためXMLHttpRequestをしてHTTPリクエストを送る。
-      // オブジェクトを生成
+      // Ajaxに必要なオブジェクトを生成している
       const XHR = new XMLHttpRequest();
-
-      // httpメソッド:GET,path指定:/posts/${postId},非同期通信のON/OFF:true
+      // openでリクエストを初期化する
       XHR.open("GET", `/posts/${postId}`, true);
-
-      // リクエストを送る際に予め、レスポンスしてほしい情報の形式を指定する。
-      // レスポンスの形式はjsonとする。
+      // レスポンスのタイプを指定する
       XHR.responseType = "json";
-
-      // .sendメソッドを記述することで初めてリクエストが行える。
+      // sendでリクエストを送信する
       XHR.send();
-
-      // checked.jsに、レスポンスがあった場合の処理
-      // XHR.responseでレスポンスされてきたJSONにアクセスできる。
+      // レスポンスを受け取った時の処理を記述する
       XHR.onload = () => {
-        // レスポンスがエラーだった場合の処理
-        // ステータスコードの200は処理成功であるが、!=であるので、処理が成功しない場合を表している
         if (XHR.status != 200) {
           // レスポンスの HTTP ステータスを解析し、該当するエラーメッセージをアラートで表示するようにしている
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
-
-          // javascriptから抜け出す
+          // 処理を終了している
           return null;          
         }
-        
         // レスポンスされたデータを変数itemに代入している
         const item = XHR.response.post;
         if (item.checked === true) {
-
-        // 既読状態であれば、灰色に変わるcssを適用するためのカスタムデータを追加している
+          // 既読状態であれば、灰色に変わるcssを適用するためのカスタムデータを追加している
           post.setAttribute("data-check", "true");
         } else if (item.checked === false) {
-        
-        // 未読状態であれば、カスタムデータを削除している
+          // 未読状態であれば、カスタムデータを削除している
           post.removeAttribute("data-check");
         }
       };
     });
   });
 }
-// ページを読み込んだらcheckを実行するのではなく、
-// window.addEventListener("load", check);
-
-// 一定時間ごとに、自動でcheckを実行する仕様にする。
 setInterval(check, 1000);
